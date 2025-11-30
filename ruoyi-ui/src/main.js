26081a3654988a2ad9ac49562c908d2e6313fd81
SpringSunYY <3217778348@qq.com>
2025-11-30 23:29:05 +0800

    优化生成获取文件路径

--- ruoyi-ui/src/main.js
+++ ruoyi-ui/src/main.js
@@ -37,7 +37,7 @@ import DictTag from '@/components/DictTag'
 import VueMeta from 'vue-meta'
 // 字典数据组件
 import DictData from '@/components/DictData'
-
+import { getFileName ,getFilePath} from '@/utils/ruoyi'
 // 全局方法挂载
 Vue.prototype.getDicts = getDicts
 Vue.prototype.getConfigKey = getConfigKey
@@ -48,6 +48,8 @@ Vue.prototype.selectDictLabel = selectDictLabel
 Vue.prototype.selectDictLabels = selectDictLabels
 Vue.prototype.download = download
 Vue.prototype.handleTree = handleTree
+Vue.prototype.getFileName = getFileName
+Vue.prototype.getFilePath = getFilePath
 
 // 全局组件挂载
 Vue.component('DictTag', DictTag)
